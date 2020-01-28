ff003578ed4d55edcf499441cd6ab05022a02660
Otman Bouchari <obouchari@gmail.com>
2020-01-28 13:24:06 +0100

    Create a "useFetch" hook to load and update the repos list.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from "react";
+import { fetchRepos, filterRepos } from "./Utilities/fetchRepos";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+function useFetch(page) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [repos, setRepos] = useState([]);
 
+  useEffect(() => {
+    setLoading(true);
+    fetchRepos(page)
+        .then(data => setRepos(repos => repos.concat(filterRepos(data))))
+        .catch(error => {
+          if (error instanceof window.Response) {
+            setError(`${error.status}: ${error.statusText}`);
+          } else {
+            setError(error.toString());
+          }
+        })
+        .finally(() => setLoading(false));
+  }, [page]);
+
+  return {
+    loading,
+    error,
+    repos
+  };
+}
+
+function App() {
+  const [page, setPage] = useState(1);
+  const { loading, error, repos } = useFetch(page);
+  return (
+      <div>
       </div>
-    );
-  }
+  );
 }
 
 export default App;
